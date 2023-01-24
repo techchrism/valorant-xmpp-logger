dd4c52b6004bcf2833a4d6e5689cb8111296e4d3
techchrism <chris@techchrism.me>
2023-01-23 23:42:31 -0800

    initial working version

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import * as fs from 'node:fs'
+import {ConfigMITM} from './ConfigMITM'
+import {XmppMITM} from './XmppMITM'
 
 (async () => {
-    try {
-        await fs.promises.mkdir('./logs')
-    }
-    catch (ignored) {}
+    const logDir = './logs'
+    const httpPort = 35479
+    const xmppPort = 35478
+    const host = '127.0.0.1'
 
-    const logPath = `./logs/${(new Date()).getTime()}.txt`
+    try {
+        await fs.promises.mkdir(logDir)
+    } catch (ignored) {}
+
+    const logPath = `${logDir}/${(new Date()).getTime()}.txt`
     console.log(`Writing to ${logPath}`)
 
     const logStream = fs.createWriteStream(logPath)
@@ -15,4 +21,11 @@ import * as fs from 'node:fs'
         type: 'valorant-xmpp-logger',
         version: '1.0.0'
     }) + '\n')
+
+    const configMitm = new ConfigMITM(httpPort, host, xmppPort)
+    await configMitm.start()
+    console.log(`Listening on ${host}:${httpPort}`)
+    const xmppMitm = new XmppMITM(xmppPort, host, configMitm, logStream)
+    await xmppMitm.start()
+    console.log('XMPP listening')
 })()
